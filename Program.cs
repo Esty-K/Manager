efe7fd9b861ab7766f71c5a6451f0d1599508fb8
214804460 <38214804460@mby.co.il>
2024-11-19 18:04:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionsString = "Data Source=SRV2\\PUPILS;Initial Catalog=214804460_Shop;Integrated Security=True";
+            ProductsDataAccess cda = new ProductsDataAccess();
+            cda.ReadData(connectionsString);
+        }
+    }
+}
